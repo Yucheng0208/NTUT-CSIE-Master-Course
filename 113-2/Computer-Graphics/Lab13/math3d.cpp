359e24e88167195413278c51753fc6647c25dce7
Yucheng0208 <45489725+Yucheng0208@users.noreply.github.com>
2025-06-04 16:31:27 +0800

    Follow the slide to update the source code

--- 113-2/Computer-Graphics/Lab13/math3d.cpp
+++ 113-2/Computer-Graphics/Lab13/math3d.cpp
@@ -63,7 +63,7 @@ float m3dGetDistanceSquared(const M3DVector3f u, const M3DVector3f v)
 	{
 	float x = u[0] - v[0];
 	x = x*x;
-	
+
 	float y = u[1] - v[1];
 	y = y*y;
 
@@ -78,7 +78,7 @@ double m3dGetDistanceSquared(const M3DVector3d u, const M3DVector3d v)
 	{
 	double x = u[0] - v[0];
 	x = x*x;
-	
+
 	double y = u[1] - v[1];
 	y = y*y;
 
@@ -158,7 +158,7 @@ void m3dMatrixMultiply44(M3DMatrix33d product, const M3DMatrix33d a, const M3DMa
 // Creates a 3x3 rotation matrix, takes radians NOT degrees
 void m3dRotationMatrix33(M3DMatrix33f m, float angle, float x, float y, float z)
 	{
-	
+
 	float mag, s, c;
 	float xx, yy, zz, xy, yz, zx, xs, ys, zs, one_c;
 
@@ -492,7 +492,7 @@ bool m3dInvertMatrix44(M3DMatrix44f dst, const M3DMatrix44f src )
 	MAT(dst,2,0) = r2[4]; MAT(dst,2,1) = r2[5],
 	MAT(dst,2,2) = r2[6]; MAT(dst,2,3) = r2[7],
 	MAT(dst,3,0) = r3[4]; MAT(dst,3,1) = r3[5],
-	MAT(dst,3,2) = r3[6]; MAT(dst,3,3) = r3[7]; 
+	MAT(dst,3,2) = r3[6]; MAT(dst,3,3) = r3[7];
 
 	return true;
 
@@ -529,13 +529,13 @@ bool m3dInvertMatrix44(M3DMatrix44d dst, const M3DMatrix44d src)
 	r3[2] = MAT(src,3,2), r3[3] = MAT(src,3,3),
 	r3[7] = 1.0, r3[4] = r3[5] = r3[6] = 0.0;
 
-	// choose pivot - or die 
+	// choose pivot - or die
 	if (fabs(r3[0])>fabs(r2[0])) SWAP_ROWS(r3, r2);
 	if (fabs(r2[0])>fabs(r1[0])) SWAP_ROWS(r2, r1);
 	if (fabs(r1[0])>fabs(r0[0])) SWAP_ROWS(r1, r0);
 	if (0.0 == r0[0])  return false;
 
-	// eliminate first variable     
+	// eliminate first variable
 	m1 = r1[0]/r0[0]; m2 = r2[0]/r0[0]; m3 = r3[0]/r0[0];
 	s = r0[1]; r1[1] -= m1 * s; r2[1] -= m2 * s; r3[1] -= m3 * s;
 	s = r0[2]; r1[2] -= m1 * s; r2[2] -= m2 * s; r3[2] -= m3 * s;
@@ -549,12 +549,12 @@ bool m3dInvertMatrix44(M3DMatrix44d dst, const M3DMatrix44d src)
 	s = r0[7];
 	if (s != 0.0) { r1[7] -= m1 * s; r2[7] -= m2 * s; r3[7] -= m3 * s; }
 
-	// choose pivot - or die 
+	// choose pivot - or die
 	if (fabs(r3[1])>fabs(r2[1])) SWAP_ROWS(r3, r2);
 	if (fabs(r2[1])>fabs(r1[1])) SWAP_ROWS(r2, r1);
 	if (0.0 == r1[1])  return false;
 
-	// eliminate second variable 
+	// eliminate second variable
 	m2 = r2[1]/r1[1]; m3 = r3[1]/r1[1];
 	r2[2] -= m2 * r1[2]; r3[2] -= m3 * r1[2];
 	r2[3] -= m2 * r1[3]; r3[3] -= m3 * r1[3];
@@ -563,23 +563,23 @@ bool m3dInvertMatrix44(M3DMatrix44d dst, const M3DMatrix44d src)
 	s = r1[6]; if (0.0 != s) { r2[6] -= m2 * s; r3[6] -= m3 * s; }
 	s = r1[7]; if (0.0 != s) { r2[7] -= m2 * s; r3[7] -= m3 * s; }
 
-	// choose pivot - or die 
+	// choose pivot - or die
 	if (fabs(r3[2])>fabs(r2[2])) SWAP_ROWS(r3, r2);
 	if (0.0 == r2[2])  return false;
 
-	// eliminate third variable 
+	// eliminate third variable
 	m3 = r3[2]/r2[2];
 	r3[3] -= m3 * r2[3], r3[4] -= m3 * r2[4],
 	r3[5] -= m3 * r2[5], r3[6] -= m3 * r2[6],
 	r3[7] -= m3 * r2[7];
 
-	// last check 
+	// last check
 	if (0.0 == r3[3]) return false;
 
-	s = 1.0f/r3[3];              // now back substitute row 3 
+	s = 1.0f/r3[3];              // now back substitute row 3
 	r3[4] *= s; r3[5] *= s; r3[6] *= s; r3[7] *= s;
 
-	m2 = r2[3];                 // now back substitute row 2 
+	m2 = r2[3];                 // now back substitute row 2
 	s  = 1.0f/r2[2];
 	r2[4] = s * (r2[4] - r3[4] * m2), r2[5] = s * (r2[5] - r3[5] * m2),
 	r2[6] = s * (r2[6] - r3[6] * m2), r2[7] = s * (r2[7] - r3[7] * m2);
@@ -590,7 +590,7 @@ bool m3dInvertMatrix44(M3DMatrix44d dst, const M3DMatrix44d src)
 	r0[4] -= r3[4] * m0, r0[5] -= r3[5] * m0,
 	r0[6] -= r3[6] * m0, r0[7] -= r3[7] * m0;
 
-	m1 = r1[2];                 // now back substitute row 1 
+	m1 = r1[2];                 // now back substitute row 1
 	s  = 1.0f/r1[1];
 	r1[4] = s * (r1[4] - r2[4] * m1), r1[5] = s * (r1[5] - r2[5] * m1),
 	r1[6] = s * (r1[6] - r2[6] * m1), r1[7] = s * (r1[7] - r2[7] * m1);
@@ -598,7 +598,7 @@ bool m3dInvertMatrix44(M3DMatrix44d dst, const M3DMatrix44d src)
 	r0[4] -= r2[4] * m0, r0[5] -= r2[5] * m0,
 	r0[6] -= r2[6] * m0, r0[7] -= r2[7] * m0;
 
-	m0 = r0[1];                 // now back substitute row 0 
+	m0 = r0[1];                 // now back substitute row 0
 	s  = 1.0f/r0[0];
 	r0[4] = s * (r0[4] - r1[4] * m0), r0[5] = s * (r0[5] - r1[5] * m0),
 	r0[6] = s * (r0[6] - r1[6] * m0), r0[7] = s * (r0[7] - r1[7] * m0);
@@ -610,7 +610,7 @@ bool m3dInvertMatrix44(M3DMatrix44d dst, const M3DMatrix44d src)
 	MAT(dst,2,0) = r2[4]; MAT(dst,2,1) = r2[5],
 	MAT(dst,2,2) = r2[6]; MAT(dst,2,3) = r2[7],
 	MAT(dst,3,0) = r3[4]; MAT(dst,3,1) = r3[5],
-	MAT(dst,3,2) = r3[6]; MAT(dst,3,3) = r3[7]; 
+	MAT(dst,3,2) = r3[6]; MAT(dst,3,3) = r3[7];
 
 	return true;
 
@@ -630,10 +630,10 @@ void m3dProjectXY(const M3DMatrix44f mModelView, const M3DMatrix44f mProjection,
 
 	memcpy(vBack, vPointIn, sizeof(float)*3);
 	vBack[3] = 1.0f;
-    
+
     m3dTransformVector4(vForth, vBack, mModelView);
     m3dTransformVector4(vBack, vForth, mProjection);
-    
+
     if(!m3dCloseEnough(vBack[3], 0.0f, 0.000001f)) {
         float div = 1.0f / vBack[3];
         vBack[0] *= div;
@@ -646,9 +646,9 @@ void m3dProjectXY(const M3DMatrix44f mModelView, const M3DMatrix44f mProjection,
 
     /* Map x,y to viewport */
     vPointOut[0] = (vPointOut[0] * iViewPort[2]) + iViewPort[0];
-    vPointOut[1] = (vPointOut[1] * iViewPort[3]) + iViewPort[1];    
+    vPointOut[1] = (vPointOut[1] * iViewPort[3]) + iViewPort[1];
 	}
-    
+
 ///////////////////////////////////////////////////////////////////////////////////////
 // Get window coordinates, we also want Z....
 void m3dProjectXYZ(const M3DMatrix44f mModelView, const M3DMatrix44f mProjection, const int iViewPort[4], const M3DVector3f vPointIn, M3DVector3f vPointOut)
@@ -657,15 +657,15 @@ void m3dProjectXYZ(const M3DMatrix44f mModelView, const M3DMatrix44f mProjection
 
 	memcpy(vBack, vPointIn, sizeof(float)*3);
 	vBack[3] = 1.0f;
-    
+
     m3dTransformVector4(vForth, vBack, mModelView);
     m3dTransformVector4(vBack, vForth, mProjection);
-    
+
     if(!m3dCloseEnough(vBack[3], 0.0f, 0.000001f)) {
         float div = 1.0f / vBack[3];
         vBack[0] *= div;
         vBack[1] *= div;
-        vBack[2] *= div; 
+        vBack[2] *= div;
         }
 
     vPointOut[0] = vBack[0] * 0.5f + 0.5f;
@@ -685,8 +685,8 @@ void m3dProjectXYZ(const M3DMatrix44f mModelView, const M3DMatrix44f mProjection
 ///////////////////////////////////////////////////////////////////////////////
 // Calculates the normal of a triangle specified by the three points
 // p1, p2, and p3. Each pointer points to an array of three floats. The
-// triangle is assumed to be wound counter clockwise. 
-void m3dFindNormal(M3DVector3f result, const M3DVector3f point1, const M3DVector3f point2, 
+// triangle is assumed to be wound counter clockwise.
+void m3dFindNormal(M3DVector3f result, const M3DVector3f point1, const M3DVector3f point2,
 							const M3DVector3f point3)
 	{
 	M3DVector3f v1,v2;		// Temporary vectors
@@ -709,7 +709,7 @@ void m3dFindNormal(M3DVector3f result, const M3DVector3f point1, const M3DVector
 
 
 // Ditto above, but for doubles
-void m3dFindNormal(M3DVector3d result, const M3DVector3d point1, const M3DVector3d point2, 
+void m3dFindNormal(M3DVector3d result, const M3DVector3d point1, const M3DVector3d point2,
 							const M3DVector3d point3)
 	{
 	M3DVector3d v1,v2;		// Temporary vectors
@@ -793,7 +793,7 @@ void m3dCatmullRom3(M3DVector3f vOut, M3DVector3f vP0, M3DVector3f vP1, M3DVecto
     float t2 = t * t;
     float t3 = t2 * t;
 
-    // X    
+    // X
     vOut[0] = 0.5f * ( ( 2.0f * vP1[0]) +
                        (-vP0[0] + vP2[0]) * t +
                        (2.0f * vP0[0] - 5.0f *vP1[0] + 4.0f * vP2[0] - vP3[0]) * t2 +
@@ -823,7 +823,7 @@ void m3dCatmullRom3(M3DVector3d vOut, M3DVector3d vP0, M3DVector3d vP1, M3DVecto
     double t2 = t * t;
     double t3 = t2 * t;
 
-    // X    
+    // X
     vOut[0] = 0.5 * ( ( 2.0 * vP1[0]) +
                        (-vP0[0] + vP2[0]) * t +
                        (2.0 * vP0[0] - 5.0 *vP1[0] + 4.0 * vP2[0] - vP3[0]) * t2 +
@@ -854,22 +854,22 @@ double m3dRaySphereTest(const M3DVector3d point, const M3DVector3d ray, const M3
 	//m3dNormalizeVector(ray);	// Make sure ray is unit length
 
 	M3DVector3d rayToCenter;	// Ray to center of sphere
-	rayToCenter[0] =  sphereCenter[0] - point[0];	
+	rayToCenter[0] =  sphereCenter[0] - point[0];
 	rayToCenter[1] =  sphereCenter[1] - point[1];
 	rayToCenter[2] =  sphereCenter[2] - point[2];
-	
+
 	// Project rayToCenter on ray to test
 	double a = m3dDotProduct(rayToCenter, ray);
-	
+
 	// Distance to center of sphere
 	double distance2 = m3dDotProduct(rayToCenter, rayToCenter);	// Or length
 
-	
+
 	double dRet = (sphereRadius * sphereRadius) - distance2 + (a*a);
-	
+
 	if(dRet > 0.0)			// Return distance to intersection
 		dRet = a - sqrt(dRet);
-	
+
 	return dRet;
 	}
 
@@ -881,48 +881,48 @@ float m3dRaySphereTest(const M3DVector3f point, const M3DVector3f ray, const M3D
 	//m3dNormalizeVectorf(ray);	// Make sure ray is unit length
 
 	M3DVector3f rayToCenter;	// Ray to center of sphere
-	rayToCenter[0] =  sphereCenter[0] - point[0];	
+	rayToCenter[0] =  sphereCenter[0] - point[0];
 	rayToCenter[1] =  sphereCenter[1] - point[1];
 	rayToCenter[2] =  sphereCenter[2] - point[2];
-	
+
 	// Project rayToCenter on ray to test
 	float a = m3dDotProduct(rayToCenter, ray);
-	
+
 	// Distance to center of sphere
 	float distance2 = m3dDotProduct(rayToCenter, rayToCenter);	// Or length
-	
+
 	float dRet = (sphereRadius * sphereRadius) - distance2 + (a*a);
-	
+
 	if(dRet > 0.0)			// Return distance to intersection
 		dRet = a - sqrtf(dRet);
-	
+
 	return dRet;
 	}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Calculate the tangent basis for a triangle on the surface of a model
-// This vector is needed for most normal mapping shaders 
+// This vector is needed for most normal mapping shaders
 void m3dCalculateTangentBasis(const M3DVector3f vTriangle[3], const M3DVector2f vTexCoords[3], const M3DVector3f N, M3DVector3f vTangent)
 {
     M3DVector3f dv2v1, dv3v1;
     float dc2c1t, dc2c1b, dc3c1t, dc3c1b;
     float M;
-    
+
     m3dSubtractVectors3(dv2v1, vTriangle[1], vTriangle[0]);
     m3dSubtractVectors3(dv3v1, vTriangle[2], vTriangle[0]);
-    
+
     dc2c1t = vTexCoords[1][0] - vTexCoords[0][0];
     dc2c1b = vTexCoords[1][1] - vTexCoords[0][1];
     dc3c1t = vTexCoords[2][0] - vTexCoords[0][0];
     dc3c1b = vTexCoords[2][1] - vTexCoords[0][1];
-    
+
     M = (dc2c1t * dc3c1b) - (dc3c1t * dc2c1b);
     M = 1.0f / M;
-    
+
     m3dScaleVector3(dv2v1, dc3c1b);
     m3dScaleVector3(dv3v1, dc2c1b);
-    
+
     m3dSubtractVectors3(vTangent, dv2v1, dv3v1);
     m3dScaleVector3(vTangent, M);  // This potentially changes the direction of the vector
     m3dNormalizeVector(vTangent);
@@ -932,8 +932,8 @@ void m3dCalculateTangentBasis(const M3DVector3f vTriangle[3], const M3DVector2f 
     m3dCrossProduct(vTangent, B, N);
     m3dNormalizeVector(vTangent);
     }
-	
-	
+
+
 ////////////////////////////////////////////////////////////////////////////
 // Smoothly step between 0 and 1 between edge1 and edge 2
 double m3dSmoothStep(double edge1, double edge2, double x)
@@ -942,10 +942,10 @@ double m3dSmoothStep(double edge1, double edge2, double x)
     t = (x - edge1) / (edge2 - edge1);
     if(t > 1.0)
         t = 1.0;
-        
+
     if(t < 0.0)
         t = 0.0f;
-        
+
     return t * t * ( 3.0 - 2.0 * t);
     }
 
@@ -957,14 +957,14 @@ float m3dSmoothStep(float edge1, float edge2, float x)
     t = (x - edge1) / (edge2 - edge1);
     if(t > 1.0f)
         t = 1.0f;
-        
+
     if(t < 0.0)
         t = 0.0f;
-        
+
     return t * t * ( 3.0f - 2.0f * t);
     }
-	
-	
+
+
 
 ///////////////////////////////////////////////////////////////////////////
 // Creae a projection to "squish" an object into the plane.
@@ -972,8 +972,8 @@ float m3dSmoothStep(float edge1, float edge2, float x)
 // to get a plane equation.
 void m3dMakePlanarShadowMatrix(M3DMatrix44f proj, const M3DVector4f planeEq, const M3DVector3f vLightPos)
 	{
-	// These just make the code below easier to read. They will be 
-	// removed by the optimizer.	
+	// These just make the code below easier to read. They will be
+	// removed by the optimizer.
 	float a = planeEq[0];
 	float b = planeEq[1];
 	float c = planeEq[2];
@@ -1005,16 +1005,16 @@ void m3dMakePlanarShadowMatrix(M3DMatrix44f proj, const M3DVector4f planeEq, con
 	proj[15] = a * dx + b * dy + c * dz;
 	// Shadow matrix ready
 	}
-	
-	
+
+
 ///////////////////////////////////////////////////////////////////////////
 // Creae a projection to "squish" an object into the plane.
 // Use m3dGetPlaneEquationd(planeEq, point1, point2, point3);
 // to get a plane equation.
 void m3dMakePlanarShadowMatrix(M3DMatrix44d proj, const M3DVector4d planeEq, const M3DVector3f vLightPos)
 	{
-	// These just make the code below easier to read. They will be 
-	// removed by the optimizer.	
+	// These just make the code below easier to read. They will be
+	// removed by the optimizer.
 	double a = planeEq[0];
 	double b = planeEq[1];
 	double c = planeEq[2];
@@ -1056,35 +1056,35 @@ void m3dMakePlanarShadowMatrix(M3DMatrix44d proj, const M3DVector4d planeEq, con
 // In: vPointInSpace is the point in space
 // Out: vPointOnRay is the poing on the ray closest to vPointInSpace
 // Return: The square of the distance to the ray
-double m3dClosestPointOnRay(M3DVector3d vPointOnRay, const M3DVector3d vRayOrigin, const M3DVector3d vUnitRayDir, 
+double m3dClosestPointOnRay(M3DVector3d vPointOnRay, const M3DVector3d vRayOrigin, const M3DVector3d vUnitRayDir,
 											const M3DVector3d vPointInSpace)
 	{
 	M3DVector3d v;
 	m3dSubtractVectors3(v, vPointInSpace, vRayOrigin);
-	
+
 	double t = m3dDotProduct(vUnitRayDir, v);
-	
+
 	// This is the point on the ray
 	vPointOnRay[0] = vRayOrigin[0] + (t * vUnitRayDir[0]);
 	vPointOnRay[1] = vRayOrigin[1] + (t * vUnitRayDir[1]);
 	vPointOnRay[2] = vRayOrigin[2] + (t * vUnitRayDir[2]);
-	
+
 	return m3dGetDistanceSquared(vPointOnRay, vPointInSpace);
 	}
 
 // ditto above... but with floats
-float m3dClosestPointOnRay(M3DVector3f vPointOnRay, const M3DVector3f vRayOrigin, const M3DVector3f vUnitRayDir, 
+float m3dClosestPointOnRay(M3DVector3f vPointOnRay, const M3DVector3f vRayOrigin, const M3DVector3f vUnitRayDir,
 							 const M3DVector3f vPointInSpace)
 	{
 	M3DVector3f v;
 	m3dSubtractVectors3(v, vPointInSpace, vRayOrigin);
-	
+
 	float t = m3dDotProduct(vUnitRayDir, v);
-	
+
 	// This is the point on the ray
 	vPointOnRay[0] = vRayOrigin[0] + (t * vUnitRayDir[0]);
 	vPointOnRay[1] = vRayOrigin[1] + (t * vUnitRayDir[1]);
 	vPointOnRay[2] = vRayOrigin[2] + (t * vUnitRayDir[2]);
-	
+
 	return m3dGetDistanceSquared(vPointOnRay, vPointInSpace);
 	}
